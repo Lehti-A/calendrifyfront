e33c0e3f031a1896b4f4659737e80904a0920ad7
kutzzar <karel.kaarma@gmail.com>
2025-03-04 16:43:16 +0200

    HomeView GIF

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -1,5 +1,118 @@
 <template>
 
+  <div class="content">
+    <div class="row justify-content-center mt-1">
+      <div class="col-md-8 d-flex flex-column align-items-center justify-content-center text-center">
+        <h2 class="fw-bold">CALENDAR</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center mt-1"> qouteAPI ph </div>
+    <div class="row justify-content-center mt-1">
+      <div class="col">
+        <ul class="list-group">
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Meditate 30min
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+              <label class="form-check-label" for="flexCheckDefault">
+                Done
+              </label>
+            </div>
+          </li><li class="list-group-item d-flex justify-content-between align-items-center">
+          Meditate 30min
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              Done
+            </label>
+          </div>
+        </li><li class="list-group-item d-flex justify-content-between align-items-center">
+          Meditate 30min
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              Done
+            </label>
+          </div>
+        </li><li class="list-group-item d-flex justify-content-between align-items-center">
+          Meditate 30min
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              Done
+            </label>
+          </div>
+        </li><li class="list-group-item d-flex justify-content-between align-items-center">
+          Meditate 30min
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              Done
+            </label>
+          </div>
+        </li>
+
+        </ul>
+      </div>
+      <div class="col">
+        <ul class="list-group">
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Meditate 30min
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+              <label class="form-check-label" for="flexCheckDefault">
+                Done
+              </label>
+            </div>
+          </li><li class="list-group-item d-flex justify-content-between align-items-center">
+          Meditate 30min
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              Done
+            </label>
+          </div>
+        </li><li class="list-group-item d-flex justify-content-between align-items-center">
+          Meditate 30min
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              Done
+            </label>
+          </div>
+        </li><li class="list-group-item d-flex justify-content-between align-items-center">
+          Meditate 30min
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              Done
+            </label>
+          </div>
+        </li><li class="list-group-item d-flex justify-content-between align-items-center">
+          Meditate 30min
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              Done
+            </label>
+          </div>
+        </li>
+
+        </ul>
+      </div>
+    </div>
+    <div class="row justify-content-center mt-1"> February 2025 ph </div>
+    <div class="row justify-content-center mt-1">
+      <div class="col"> January 2025 ph
+      </div>
+      <div class="col"> March 2025 ph
+      </div>
+
+    </div>
+    <div class="row justify-content-center mt-1"> kalenderAPI ph </div>
+
+  </div>
+
 
 
 
