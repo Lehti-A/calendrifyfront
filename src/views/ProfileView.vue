d3d71ef3ebda2f3cabf8647675244da8d29105b8
kutzzar <karel.kaarma@gmail.com>
2025-03-06 11:51:22 +0200

    CALENDARVIEW WIP

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,143 @@
+<template>
+
+  <div>
+
+    <div class="background-image">
+      <img :src="backgroundImage" alt="Background Image" />
+    </div>
+
+    <div class="registration-container d-flex justify-content-center align-items-center vh-100">
+      <div class="card p-4 rounded shadow-sm" style="max-width: 400px; background-color: white;">
+        <h2 class="text-center mb-4">Edit Profile</h2>
+
+
+        <form>
+          <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input
+                type="email"
+                class="form-control"
+                v-model="newUser.email"
+                placeholder="Enter your email"
+                required
+            />
+          </div>
+
+
+          <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input
+                type="password"
+                class="form-control"
+                v-model="newUser.password"
+                placeholder="Enter your password"
+                required
+            />
+          </div>
+
+
+          <div class="mb-3">
+            <label for="retypePassword" class="form-label">Retype Password</label>
+            <input
+                type="password"
+                class="form-control"
+                v-model="passwordRetype"
+                placeholder="Retype your password"
+                required
+            />
+          </div>
+
+
+          <div class="mb-3">
+            <label for="address" class="form-label">Address</label>
+            <input
+                type="text"
+                class="form-control"
+                v-model="newUser.address"
+                placeholder="Enter your address"
+                required
+            />
+          </div>
+
+
+          <div class="mb-3">
+            <label for="phone" class="form-label">Phone</label>
+            <input
+                type="tel"
+                class="form-control"
+                v-model="newUser.phone"
+                placeholder="Enter your phone number"
+                required
+            />
+          </div>
+
+          <div class="d-flex justify-content-between">
+            <button type="button" class="btn btn-secondary flex-grow-1 me-2" @click="">Delete</button>
+            <button type="button" class="btn btn-secondary flex-grow-1 me-2" @click="">Update</button>
+
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserService from "@/services/UserService";
+import NavigationServices from "@/services/NavigationServices";
+
+export default {
+  name: "RegisterView",
+  data() {
+    return {
+      backgroundImage: require('@/assets/calendrifynew.gif'),
+      passwordRetype:'',
+      errorMessage: '',
+      newUser: {
+        email: '',
+        password: '',
+        address: '',
+        phone: '',
+        termsAgreed: false,
+      },
+    };
+  },
+  methods: {
+    addNewUser(){
+      if(this.passwordNoMatch()){
+        this.errorMessage = "Paroolid ei kattu"
+      } else {
+        UserService.sendPostNewUserRequest(this.newUser)
+            .then(() => NavigationServices.navigateToHomeView())
+            .catch(() => NavigationServices.navigateToErrorView())
+
+      }
+    },
+
+    passwordNoMatch() {
+      return this.passwordRetype !== this.newUser.password
+    },
+
+
+
+
+
+
+
+
+
+    navigateBack(){
+      this.$router.go(-1)
+    }
+
+
+  },
+}
+
+</script>
+
+<style src="@/assets/background.css"></style>
+
+
+
+
