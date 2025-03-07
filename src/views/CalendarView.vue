b314998f41830b652751dfc4703ed5aff59300a8
kutzzar <karel.kaarma@gmail.com>
2025-03-07 11:25:11 +0200

    WIP

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -1,40 +1,57 @@
 <template>
   <div class="content">
     <CalendarModal :modal-is-open="modalIsOpen"
-                @event-close-modal="closeCalendarModal"
-                @event-update-nav-menu="$emit('event-update-nav-menu')"
+                   @event-close-modal="closeCalendarModal"
+                   @event-update-nav-menu="$emit('event-update-nav-menu')"
     />
 
     <div class="row justify-content-center mt-3">
-      <div class="col-md-8 d-flex flex-column align-items-center justify-content-center text-center"> </div>
-        <h2 class="fw-bold">CALENDAR</h2>
-      <div class="col text-center">
+      <div class="col d-flex flex-column align-items-center justify-content-center text-center"> </div>
+      <h2 class="fw-bold">CALENDAR</h2>
+      <div class="col text-center mb-3" style="max-width: 600px; width: 100%;">
         {{ quote }}
       </div>
     </div>
-      <button type="button" class="btn btn-secondary mx-3" style="width: 7%;" @click="fetchData">New Quote</button>
-      <button type="button" class="btn btn-secondary" style="width: 7%;" @click="">Search day</button>
-    <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle mt-3" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        Month
-      </button>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-      </ul>
+    <button type="button" class="btn btn-secondary mx-3" style="width: 7%;" @click="fetchData">New Quote</button>
+    <button type="button" class="btn btn-secondary" style="width: 7%;" @click="">Search day</button>
+
+
+    <div class="row justify-content-center mt-3">
+      <div class="col d-flex justify-content-center gap-2">
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle mx-3" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Month
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">January</a></li>
+            <li><a class="dropdown-item" href="#">February</a></li>
+            <li><a class="dropdown-item" href="#">March</a></li>
+            <li><a class="dropdown-item" href="#">April</a></li>
+            <li><a class="dropdown-item" href="#">May</a></li>
+            <li><a class="dropdown-item" href="#">June</a></li>
+            <li><a class="dropdown-item" href="#">July</a></li>
+            <li><a class="dropdown-item" href="#">August</a></li>
+            <li><a class="dropdown-item" href="#">September</a></li>
+            <li><a class="dropdown-item" href="#">October</a></li>
+            <li><a class="dropdown-item" href="#">November</a></li>
+            <li><a class="dropdown-item" href="#">December</a></li>
+          </ul>
+        </div>
+
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Year
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">2024</a></li>
+            <li><a class="dropdown-item" href="#">2025</a></li>
+            <li><a class="dropdown-item" href="#">2026</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
-    <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle mt-3" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        Year
-      </button>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-      </ul>
-    </div>
-        <div class="row justify-content-center mt-1">
+
+    <div class="row justify-content-center mt-1">
       <div class="col" style="width: 800px;" > Personal Focus
         <ul class="list-group">
           <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -106,5 +123,3 @@ export default {
   }
 }
 </script>
-
-
